e3410f06d349907063182452f0ca8431a0ad90ac
Hackerman <marcos.p.pollastri@gmail.com>
2023-06-21 19:54:03 -0300

    patch: salto de linea

--- cowsay.js
+++ cowsay.js
@@ -1,7 +1,7 @@
 const cowsay = require('cowsay');
 
 const message = cowsay.say({
-    text : "Hola PilarTecno #4 Soy una vaquita",
+    text : "Hola PilarTecno #4\nUn paso para el hombre y un salto de línea para la humanidad",
     e : "oO",
     T : "U "
 });
