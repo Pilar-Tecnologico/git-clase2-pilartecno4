18f6932f6e04bf4f41815e0a51c7152981740999
Marcos J. Peña Pollastri <marcos.p.pollastri@gmail.com>
2023-06-26 19:26:37 -0300

    Revert "patch: salto de linea"
    This reverts commit e3410f06d349907063182452f0ca8431a0ad90ac.

--- cowsay.js
+++ cowsay.js
@@ -1,7 +1,7 @@
 const cowsay = require('cowsay');
 
 const message = cowsay.say({
-    text : "Hola PilarTecno #4\nUn paso para el hombre y un salto de línea para la humanidad",
+    text : "Hola PilarTecno #4 Soy una vaquita",
     e : "oO",
     T : "U "
 });
